d47af53435d3158167229515b14098cd9b98fa35
Nandhu125 <dharnanandhu968@gmail.com>
2025-08-18 14:26:11 +0530

    updates

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  
   images: {
     domains: ["images.unsplash.com","www.shutterstock.com", "arizonagestionenergy.com"],
   },
